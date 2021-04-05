75cf40f734a80fdf2816d1993588cca0c50c1302
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-04-05 16:09:06 +0200

    Special functions for Employee class

--- cpp-examples/move.cpp
+++ cpp-examples/move.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <string>
+#include <utility>
+
+struct Employee {
+  std::string name;
+  int id;
+
+  Employee(std::string name_, int id_) : name{name_}, id{id_} {};
+
+  // Copy constructor
+  Employee(const Employee &empl) : name{empl.name}, id{empl.id} {
+    std::cout << "COPY: " << name << ", " << id << '\n';
+  }
+
+  // Copy assignment operator
+  Employee &operator=(const Employee &empl) {
+    std::cout << "COPY ASSIGNMENT: " << name << ", " << id << '\n';
+    name = empl.name;
+    id = empl.id;
+    return *this;
+  }
+
+  // Move constructor
+  Employee(Employee &&empl) noexcept
+      : name{std::move(empl.name)}, id{std::move(empl.id)} {
+    std::cout << "MOVE: " << name << ", " << id << '\n';
+  }
+
+  // Move assignment operator
+  Employee &operator=(Employee &&empl) noexcept {
+    std::cout << "MOVE ASSIGNMENT: " << name << ", " << id << '\n';
+    name = std::move(empl.name);
+    id = std::move(empl.id);
+    return *this;
+  }
+};
 
 int main() {
-    return 0;
+  auto hendrik = Employee("Hendrik", 101);
+  const auto hendrik2 = std::move(hendrik); // move
+  return 0;
 }
