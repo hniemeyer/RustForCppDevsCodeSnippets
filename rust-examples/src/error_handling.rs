23af9f5ddf7219fd4107f0b06a8fc1785f3bb507
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-04-14 12:43:54 +0200

    Anyhow and ? operator
    open non existing file and return an anyhow::Error

--- rust-examples/src/error_handling.rs
+++ rust-examples/src/error_handling.rs
@@ -1,3 +1,17 @@
+use anyhow::Result;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn open_file() -> Result<String> {
+    let mut file = File::open("foo.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
 fn main() {
-    println!("Hello World!");
+    match open_file() {
+        Ok(file) => println!("{}", file),
+        Err(error) => panic!("Problem retrieving file contents: {:?}", error),
+    };
 }
