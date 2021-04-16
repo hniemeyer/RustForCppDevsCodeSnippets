2316237f86d1e900ef3ea96ca5b53868bcb5480c
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-04-16 18:46:14 +0200

    Quadratic equation solver in Rust with basic pattern matching

--- rust-examples/src/patternmatching.rs
+++ rust-examples/src/patternmatching.rs
@@ -1,3 +1,21 @@
+enum QuadraticSolution {
+    NoSolution,
+    OneSolition(f64),
+    TwoSolutions(f64, f64),
+}
+
+fn solve_quadratic_equation(a: f64, b: f64, c: f64) -> QuadraticSolution {
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant <  0.0 { QuadraticSolution::NoSolution }
+    else if discriminant == 0.0 { QuadraticSolution::OneSolition(-b / (2.0 * a))} 
+    else { QuadraticSolution::TwoSolutions((-b + discriminant.sqrt()) / (2.0 * a), (-b - discriminant.sqrt()) / (2.0 * a))}
+}
+
 fn main() {
-    
+    let res = solve_quadratic_equation(1.0, 3.0, 1.0);
+    match res {
+        QuadraticSolution::NoSolution => println!("No solution"),
+        QuadraticSolution::OneSolition(x) => println!("One solution: {}", x),
+        QuadraticSolution::TwoSolutions(x,y) => println!("Two solutions: {}, {}", x,y)
+    }
 }
