a1ab932622cc13022b8a811409ec646acd2001ae
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-04-21 23:48:57 +0200

    modernize c++

--- cpp-examples/mutability.cpp
+++ cpp-examples/mutability.cpp
@@ -1,31 +1,32 @@
 #include <vector>
 #include <iostream>
 
-void print_shopping_list(std::vector<std::string>* shopping_list) {
-    if (shopping_list->size() == 0) {
-        std::cout << "\t* nothing\n";
+void print_shopping_list(std::vector<std::string> const &shopping_list) {
+    //                                            ^^^^^ explicitly immmutable
+    if (shopping_list.size() == 0) {
+        std::cout << "\t-- nothing\n";
     }
 
-    for (auto item = shopping_list->begin(); item != shopping_list->end(); ++item) {
-        std::cout << "\t* " << *item << '\n';
+    for (std::string item: shopping_list) {
+        std::cout << "\t* " << item << '\n';
     }
 }
 
 void buy_groceries(std::vector<std::string> shopping_list) {
     //             ^^^^^^^^^^^^^^^^^^^^^^^^ mutable by default
     std::cout << "Going out to buy:\n";
-    print_shopping_list(&shopping_list);
+    print_shopping_list(shopping_list);
 
     shopping_list.clear();
 
     std::cout << "things left to buy:\n";
-    print_shopping_list(&shopping_list);
+    print_shopping_list(shopping_list);
 }
 
 int main() {
     std::vector<std::string> shopping_list {"Pasta", "Milk", "Toilet Paper"};
     shopping_list.push_back("Chocolate");
-    //            ^^^^^^^^^^^^^^^^^^^^^ mutable by default
+    //^^^^^^^^^^^^^^^^^^^^^ mutable by default
 
     buy_groceries(shopping_list);
 }
